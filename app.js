49bf197f07779cfba5aae72a508ff11317e90104
Zachary Pataky <zacharypataky@gmail.com>
2023-02-25 19:14:12 -0500

    Started working on the "Education" section.
    * Honors and Awards
    * Course Highlights
    * Major Projects

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 // Tabbing block for "Work Experience"
 
-let tabLinks = document.getElementsByClassName('tab-link');
-let tabContents = document.getElementsByClassName('tab-contents');
+let tabLinks_edu = document.getElementsByClassName('tab-link-edu');
+let tabContents_edu = document.getElementsByClassName('tab-contents-edu');
 
-function openTab(tabName) {
-    for (tabLink of tabLinks) {
-        tabLink.classList.remove('active-link');
+function openTab_edu(tabName) {
+    for (tabLink_edu of tabLinks_edu) {
+        tabLink_edu.classList.remove('active-link-edu');
     }
-    for (tabContent of tabContents) {
-        tabContent.classList.remove('active-tab');
+    for (tabContent_edu of tabContents_edu) {
+        tabContent_edu.classList.remove('active-tab-edu');
     }
 
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabName).classList.add('active-tab');
+    event.currentTarget.classList.add('active-link-edu');
+    document.getElementById(tabName).classList.add('active-tab-edu');
+}
+
+// Tabbing block for "Work Experience"
+
+let tabLinks_we = document.getElementsByClassName('tab-link-we');
+let tabContents_we = document.getElementsByClassName('tab-contents-we');
+
+function openTab_we(tabName) {
+    for (tabLink_we of tabLinks_we) {
+        tabLink_we.classList.remove('active-link-we');
+    }
+    for (tabContent_we of tabContents_we) {
+        tabContent_we.classList.remove('active-tab-we');
+    }
+
+    event.currentTarget.classList.add('active-link-we');
+    document.getElementById(tabName).classList.add('active-tab-we');
 }
