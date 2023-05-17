fd7eb20fc8aa1adfdc82d4d4a53d5e245a83fa98
Zachary Pataky <zacharypataky@gmail.com>
2023-05-16 20:32:40 -0400

    Pimped out my edcucation section; this is looking REALLY nice

--- js/script.js
+++ js/script.js
@@ -31,8 +31,26 @@ const obs = new IntersectionObserver(
 obs.observe(headerEl);
 
 //////////////////////////////////////////////////////////////
+// Education - Tab Links
+const tabLinks = document.getElementsByClassName('tab-link');
+const tabContents = document.getElementsByClassName('tab-contents');
+
+function openTab(tabName) {
+    for (let tabLink of tabLinks) {
+        tabLink.classList.remove('tab-link--active');
+    }
+    for (let tabContent of tabContents) {
+        tabContent.classList.remove('tab-contents--active');
+    }
+
+    event.currentTarget.classList.add('tab-link--active');
+    event.stopPropagation();
+    document.getElementById(tabName).classList.add('tab-contents--active');
+}
+
+//////////////////////////////////////////////////////////////
 // Work Experience - Accordion
-const workExpAccItem = document.querySelectorAll(".accordion-item");
+let workExpAccItem = document.querySelectorAll(".accordion-item");
 workExpAccItem.forEach(workAccItem => {
     workAccItem.addEventListener("click", () => {
         workAccItem.classList.toggle('accordion-item--open');
@@ -41,4 +59,4 @@ workExpAccItem.forEach(workAccItem => {
         itemIcon.name = (itemIcon.name === 'chevron-down-outline')
             ? 'chevron-up-outline' : 'chevron-down-outline';
     })
-})
+});
