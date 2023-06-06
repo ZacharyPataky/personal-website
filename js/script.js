b54f105a9e3c1773c1e623bcca5dcf0db336eb3a
Zachary Pataky <zacharypataky@gmail.com>
2023-06-06 07:47:31 -0400

    Enabled smooth scrolling and the toggling of the mobile nav menu when navigating from it

--- js/script.js
+++ js/script.js
@@ -31,6 +31,35 @@ const obs = new IntersectionObserver(
 obs.observe(headerEl);
 
 //////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const mobileNavLinks = document.querySelectorAll(".main-nav-link");
+
+mobileNavLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        // Scroll back to top
+        if (href === "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+
+        // Scroll to other links
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+
+        // Close mobile naviagtion
+        if (link.classList.contains("main-nav-link"))
+            headerEl.classList.toggle("nav-open");
+    });
+});
+
+//////////////////////////////////////////////////////////////
 // Education - Tab Links
 const tabLinks = document.getElementsByClassName('tab-link');
 const tabContents = document.getElementsByClassName('tab-contents');
